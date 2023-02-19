fa7bf1dc79e09b3deef9c61eb61f52e540d44977
Pratibha Kumar <pratibhakumar01@gmail.com>
2023-02-18 22:36:41 -0500

    Replaced while with for loop

--- Python Exercise/Example002.py
+++ Python Exercise/Example002.py
@@ -7,7 +7,14 @@ Created on Thu Feb 16 21:28:27 2023
 print('Printing current and previous number sum in a range(10)')
 count = 0
 previous = 0
-while (count < 10):
+"""while (count < 10):
    print ("The current number ", count, "previous number ", previous , "Sum :", count + previous)
    previous = count
    count = count + 1
+print(count)"""
+count = 0
+previous = 0
+for count in range(0,10):
+   print ("The current number ", count, "previous number ", previous , "Sum :", count + previous)
+   previous = count
+   count = count + 1  

2372c6a808464bf1bc3c82c5a366623b92954c1d
Pratibha Kumar <pratibhakumar01@gmail.com>
2023-02-20 21:25:47 -0500

    used different loop

--- Python Exercise/Example006.py
+++ Python Exercise/Example006.py
@@ -5,11 +5,21 @@ Created on Mon Feb 20 09:02:40 2023
 @author: prati
 """
 
-def divisible_by_5(List):
-    for i in range(len(List)):
-        if List[i]%5==0:
-            print(List[i])
+#def divisible_by_5(List):
+    
             
 List=[10,20,33,46,55]
-divisible_by_5(List)
-    
+print(List)
+print('Divisible by 5')
+
+for num in List:
+    if num%5==0:
+        print(num)
+
+
+
+
+"""for i in range(len(List)):
+    if List[i]%5==0:
+        print(List[i])
+    """

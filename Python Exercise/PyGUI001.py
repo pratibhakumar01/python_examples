92d9bb12ac5250367b22b7477f133dbd5a97a5d2
Pratibha Kumar <pratibhakumar01@gmail.com>
2023-02-21 19:47:44 -0500

    Basic GUI window

---
+++ Python Exercise/PyGUI001.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# create root window
+root = Tk()
+ 
+# root window title and dimension
+root.title("Welcome to Pratibha's Page")
+# Set geometry (widthxheight)
+root.geometry('550x400')
+
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()

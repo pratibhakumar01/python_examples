e1689b9162675bc16720fc1c0841f766f771ce8f
Pratibha Kumar <pratibhakumar01@gmail.com>
2023-02-21 23:26:42 -0500

    Example008 - to print number in a triangle format Example009 - to find if a number is palindrome

---
+++ Python Exercise/Example008.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 20:32:54 2023
+
+@author: prati
+"""
+
+for i in range(0,10):
+    for j in range(0,i):
+        print(i, end=" ")
+    print("")  
+        
---
+++ Python Exercise/Example009.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 23:09:00 2023
+
+@author: prati
+"""
+def reverse(n):
+    rev = 0
+    while n > 0:
+        rev = rev * 10 + n % 10
+        n = n // 10
+    return rev
+
+num = int(input("Enter your number to check Palindrome: "))
+
+revnum = reverse(num)
+
+if num - revnum ==0:
+    print(num," is a Palindrome number")
+else:
+    print(num," is not a Palindrome number")
+
+#logic to reverse the number

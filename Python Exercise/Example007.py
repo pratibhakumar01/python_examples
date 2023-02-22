a5b54e11e7560d26f8f297759a39c32c66b2756e
Pratibha Kumar <pratibhakumar01@gmail.com>
2023-02-21 20:32:11 -0500

    Return the count of a given substring from a string

---
+++ Python Exercise/Example007.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 20:07:24 2023
+
+@author: prati
+"""
+
+str_x = "Emma is a girl. Emma is good writer too. Emma has own coffee shop."
+cnt = str_x.count("Emma")
+print(cnt)
+
